70d6504336b84fe32f617d84cfc559dca2962603
bikash1376 <bikash13763@gmail.com>
2025-05-27 23:49:17 +0530

    added ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import Notes from "./components/Notes";
+// import Notes from "./components/Notes";
+
+import Button from "./components/ui/Button";
+import Card from "./components/ui/Card";
 
 function App() {
   return (
     <>
-      <Notes/>
+      {/* <Notes/>
+       */}
+
+       {/* <div className="flex gap-4 p-6"> */}
+      {/* <Button text="Primary" color="primary" size="medium" /> */}
+      {/* <Button text="Primary" color="secondary" size="large" />
+      <Button text="Primary" color="success" size="small" /> */}
+
+      <Card text="Not Bikash" color="bg-gray-900" textColor="text-white" textSize="large" />
+    {/* </div> */}
+
     </>
   );
 }
